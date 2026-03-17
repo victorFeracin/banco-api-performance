3ca327ef657a1988ee624ef0726a1fb2fa9880bb
victorFeracin <victorhugo.feracin@gmail.com>
2026-03-17 01:27:29 -0300

    Adding stages to run the performance test for login endpoint

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,8 +2,12 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 10, // Virtual users
-  duration: '30s',
+  stages: [
+    { duration: '5s', target: 20 },
+    { duration: '20s', target: 15 },
+    { duration: '5s', target: 0 },
+  ],
+
   thresholds: {
     http_req_duration: ['p(90)<2000', 'max<5000'], // 95% of requests should be below 2000ms and the maximum duration should be below 5000ms
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
